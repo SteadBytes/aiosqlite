3a08d7de1e80639ad5338ca635328cce2889bd87
Ben Steadman <steadmanben1@gmail.com>
2020-09-07 19:06:18 +0100

    Bump version
    Signed-off-by: Ben Steadman <steadmanben1@gmail.com>

--- aiosqlite/__init__.py
+++ aiosqlite/__init__.py
@@ -20,7 +20,7 @@ from sqlite3 import (  # pylint: disable=redefined-builtin
 
 from .core import Connection, Cursor, connect
 
-__version__ = "0.15.0"
+__version__ = "0.15.1"
 __all__ = [
     "__version__",
     "register_adapter",
